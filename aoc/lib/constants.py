267795a926cbe10c01eb26a9b3fda5682f814866
Joerg Neulist <wsl.yoga@joerg.neulist.de>
2025-01-01 16:08:15 +0100

    reorganise project

--- aoc/lib/constants.py
+++ aoc/lib/constants.py
@@ -1,5 +1,5 @@
 DATA_URL = 'https://adventofcode.com/{}/day/{}/input'
-DATA_PATH = '../data/{:04}/{:02}'
+DATA_PATH = '../../data/{:04}/{:02}'
 HTTP_HEADER = {'User-Agent': 'AoC Loader by Joerg Neulist',
                'From': 'aoc@joerg.neulist.de'}
 SESSION_FILE = '../session'
